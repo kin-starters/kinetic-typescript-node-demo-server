248bbbd4f7ded36e2d53caf458015b1203548c36
Richard Mands <richard@devwell.org>
2021-12-29 15:48:47 +0000

    Create Account / Healthcheck

--- README.md
+++ README.md
@@ -6,3 +6,32 @@ Simple boilerplate with TypeScript, Prettier, and ESLint configured.
 Install node modules: `npm install`
 
 Run the code with auto restart: `npm run dev`
+
+
+---
+
+Kin Issues
+
+https://developer.kin.org/tutorials/node/
+```
+https://github.com/kintegrate/kin-starter-node/blob/main/src/kin.ts
+
+import { Environment, kinToQuarks, PrivateKey, PublicKey, TransactionType } from '@kin-sdk-next/base'
+import { Client } from '@kin-sdk-next/node'
+
+not
+
+import {
+  Client,
+  Environment,
+  kinToQuarks,
+  PrivateKey,
+  PublicKey,
+  TransactionType,
+} from '@kinecosystem/kin-sdk-v2';
+
+```
+```
+typo
+extend it to suite your custom needs
+```
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+
+import { Kin } from './kin';
 console.log('TypeScript Eslint Prettier Starter Template!');
 console.log('A project by Caelin Sutch, follow him at @caelin_sutch');
+
+const app = express();
+app.use(cors());
+
+const port = 3001;
+
+app.get('/healthcheck', (req, res) => {
+  console.log('🚀 ~ /healthcheck');
+  res.sendStatus(200);
+});
+
+async function createKinAccount(res) {
+  const key = await Kin.generateKey();
+  console.log('🚀 ~ key', key);
+  res.send('okay!');
+}
+app.post('/account', (req, res) => {
+  console.log('🚀 ~ /account');
+  createKinAccount(res);
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  res.sendStatus(404);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
